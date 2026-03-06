ded26b774eb300c7be941ed843228a76168264c9
Απόστολος Πεϊνίρης <apostolos.peiniris@gmail.com>
2026-03-06 23:56:14 +0200

    Update

--- models/Recipe.js
+++ models/Recipe.js
@@ -2,18 +2,16 @@ const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    chef: { type: String, default: 'Άκης Πετρετζίκης' },
     image: { type: String },
-    time: { type: Number },
+    time: { type: Number }, // Χρόνος προετοιμασίας σε λεπτά
     calories: { type: Number },
-    cost: { type: Number },
-    isHealthy: { type: Boolean },
-    isBudget: { type: Boolean },
-    ovenTemp: { type: Number, default: 180 },
-    ovenTime: { type: Number, default: 30 },
-    ingredients:[{ type: String }],
-    instructions: [{ type: String }], // 🟢 ΝΕΟ: Οι οδηγίες βήμα-βήμα
-    url: { type: String, unique: true }
+    protein: { type: Number }, // Γραμμάρια πρωτεΐνης
+    carbs: { type: Number },   // Γραμμάρια υδατανθράκων
+    fat: { type: Number },     // Γραμμάρια λιπαρών
+    isHealthy: { type: Boolean, default: true },
+    ingredients: [{ type: String }],
+    instructions: [{ type: String }], // Βήμα-βήμα εκτέλεση
+    url: { type: String, unique: true } // Για να μην διπλοεγγράφουμε
 }, { timestamps: true });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
